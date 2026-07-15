6e5c346ffc0b81a92f2ac60b8248026e344aa843
Ruben V <rubenvitorino96@gmail.com>
2026-07-15 18:10:13 +0100

    clean: remove temporary test files and update gitignore

--- test-process-env.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require 'vendor/autoload.php';
-use Symfony\Component\Process\Process;
-
-$process = Process::fromShellCommandline("/usr/bin/node -e 'console.log(process.env)'");
-$process->run();
-echo $process->getOutput();
--- test-process.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require 'vendor/autoload.php';
-use Symfony\Component\Process\Process;
-
-$process = Process::fromShellCommandline("PATH=\$PATH:/usr/local/bin:/opt/homebrew/bin NODE_PATH=\"/home/owy/Coding/Estagio/erp-inovcorp/node_modules\" \"/usr/bin/node\" '/home/owy/Coding/Estagio/erp-inovcorp/vendor/spatie/browsershot/src/../bin/browser.cjs' '{\"url\":\"file:///tmp/dummy.html\",\"action\":\"pdf\",\"options\":{\"args\":[],\"viewport\":{\"width\":800,\"height\":600},\"displayHeaderFooter\":false,\"executablePath\":\"/home/owy/Coding/Estagio/erp-inovcorp/.cache/puppeteer/chrome/linux-150.0.7871.24/chrome-linux64/chrome\"}}'");
-$process->run();
-echo $process->getErrorOutput();
-echo $process->getOutput();
